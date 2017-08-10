89351d76490525973c2a0ecab460543b318991a6
malikme3 <zulifqar.ahmad@hotmail.com>
2017-08-10 10:14:38 -0500

    Update batting.component.ts

--- src/app/pages/matches/components/submit.score/totals/batting/batting.component.ts
+++ src/app/pages/matches/components/submit.score/totals/batting/batting.component.ts
@@ -22,9 +22,10 @@ export class SubmitScoreBattingComponent {
   public name: AbstractControl;
   public gameId: AbstractControl;
   public inningsId: AbstractControl;
-  public wickets: AbstractControl;
-  public overs: AbstractControl;
-  public total: AbstractControl;
+  public runs: AbstractControl;
+  public balls: AbstractControl;
+  public fours: AbstractControl;
+  public sixes: AbstractControl;
 
   constructor(fb: FormBuilder, private matchesService: MatchesService, private matchesConstants: MatchesConstants) {
 
@@ -33,17 +34,19 @@ export class SubmitScoreBattingComponent {
       'name': ['', Validators.compose([Validators.required, Validators.minLength(4)])],
       'gameId': ['', Validators.compose([Validators.required, Validators.minLength(3)])],
       'inningsId': ['', Validators.compose([Validators.required, Validators.minLength(3)])],
-      'wickets': ['', Validators.compose([Validators.required, Validators.minLength(3)])],
-      'overs': ['', Validators.compose([Validators.required, Validators.minLength(3)])],
-      'total': ['', Validators.compose([Validators.required, Validators.minLength(3)])],
+      'runs': ['', Validators.compose([Validators.required, Validators.minLength(3)])],
+      'balls': ['', Validators.compose([Validators.required, Validators.minLength(3)])],
+      'fours': ['', Validators.compose([Validators.required, Validators.minLength(3)])],
+      'sixes': ['', Validators.compose([Validators.required, Validators.minLength(3)])],
     });
 
     this.name = this.battingForm.controls['name'];
     this.gameId = this.battingForm.controls['gameId'];
     this.inningsId = this.battingForm.controls['inningsId'];
-    this.wickets = this.battingForm.controls['wickets'];
-    this.overs = this.battingForm.controls['overs'];
-    this.total = this.battingForm.controls['total'];
+    this.runs = this.battingForm.controls['runs'];
+    this.balls = this.battingForm.controls['balls'];
+    this.fours = this.battingForm.controls['fours'];
+    this.sixes = this.battingForm.controls['sixes'];
 
   }
 
