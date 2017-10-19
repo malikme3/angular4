858fc29f32bf51b42eca4e8b0bf01de1f6d1e692
Zulifqar Ahmad <zulifqar.ahmad@hotmail.com>
2017-10-19 17:44:20 -0500

    Issues with upgrad nvm. node client fix

--- src/app/theme/components/baAmChart/baAmChart.component.ts
+++ src/app/theme/components/baAmChart/baAmChart.component.ts
@@ -1,14 +1,7 @@
+/* tslint:disable */
 import {Component, ViewChild, Input, Output, ElementRef, EventEmitter} from '@angular/core';
 
 import {BaThemePreloader} from '../../../theme/services';
-
-import 'amcharts3';
-import 'amcharts3/amcharts/plugins/responsive/responsive.js';
-import 'amcharts3/amcharts/serial.js';
-
-import 'ammap3';
-import 'ammap3/ammap/maps/js/worldLow';
-
 
 import {BaAmChartThemeService} from './baAmChartTheme.service';
 
